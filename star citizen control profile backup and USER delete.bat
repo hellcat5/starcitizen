c6ad90ebfd1f471b5ca88cde877a4ce1ac3187ea
Richard Miller <richardbmiller2@yahoo.com>
2019-04-12 22:45:41 -0400

    updated .bat files

--- star citizen control profile backup and USER delete.bat
+++ star citizen control profile backup and USER delete.bat
@@ -5,17 +5,20 @@
 :: lets tell the user what we will do, step by step
 echo prepairing to move catspaw
 
+:: the below line changes to the directory for the custom control profile
 :: change the below path to your star citizen directory for PTU or PU.  You can make two files with appropriate naming, one each for PTU and PU.
 cd C:\Program Files\Roberts Space Industries\StarCitizenPTU\LIVE\USER\Controls\Mappings
 :: you can comment out the below with :: to speed up the process if you don't want the feedback
 pause
 
+:: moves the custom control profile to a temporary place.  you can change this to anywhere you'd like.
 move "layout_catspaw_exported.xml" "C:\Users\Public\Desktop"
 
 :: lets tell the user (that's you) that we're now going to delete the USER folder in the Star Citizen directory
 echo preparing to delete the Star Citizen USER directory
 
 :: lets change the directory to where the USER folder sits
+:: change the below path to your star citizen directory for PTU or PU.  You can make two files with appropriate naming, one each for PTU and PU.
 cd C:\Program Files\Roberts Space Industries\StarCitizenPTU\LIVE
 :: you can comment out the below with :: to speed up the process if you don't want the feedback
 pause
